e327cc7c6af8365b9bd0d1ce48e3b3d179f24ce3
rlossec <raphaelossec@gmail.com>
2026-09-11 14:07:10 +0200

    [feat] Add handling schema

--- sql/01-init.sql
+++ sql/01-init.sql
@@ -40,8 +40,19 @@ DO $$
         END IF;
 END $$;
 
+
+-- Revoque les droits par défaut sur la base de données et accorde la connnexion à sacha
+REVOKE ALL ON DATABASE trombifunscope FROM PUBLIC;
+GRANT CONNECT ON DATABASE trombifunscope TO sacha;
+
 \c trombifunscope;
 
 SET ROLE admin;
 
-GRANT CONNECT ON DATABASE trombifunscope TO sacha;
+-- Personne ne peut crée d'objet dans le schéma public, sauf admin et le superutilisateur
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+GRANT USAGE ON SCHEMA public TO sacha;
+
+-- Sacha peut lire et écrire dans toutes les tables que créera admin, rien de plus
+ALTER DEFAULT PRIVILEGES FOR ROLE admin IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO sacha;
